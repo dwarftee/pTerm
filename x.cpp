7868fe443f4fb952cd2d0243ef5a0e59d40f6bec
dwarfŧ <109460706+dwarftee@users.noreply.github.com>
2023-02-15 07:57:56 +0000

    commit :0

--- x.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main() {
-        std::cout << "                    :P TERM\n";
-        std::cout << "        made by sharpi1(sharpi) and use is completely free\n";
-        std::cout << "\n";
-        std::cout << "\n";
-        std::cout << "to escape run ^C\n";
-        std::cout << "\n";
-}
